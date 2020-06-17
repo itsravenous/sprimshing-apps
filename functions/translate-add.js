054b2361b51501dbe8379b2855f6b2e324d861ec
Tom Jardine-McNamara <tom@itsravenous.com>
2020-06-17 23:09:02 +0100

    add translate adder

---
+++ functions/translate-add.js
@@ -0,0 +1,44 @@
+const { appendToSheet } = require("@itsravenous/google-sheets-private");
+const {
+  RITUNA_SHEET_ID: SHEET_ID,
+  GOOGLE_SERVICE_ACCOUNT,
+  GOOGLE_CREDENTIALS
+} = process.env;
+
+const serviceAccount = JSON.parse(GOOGLE_SERVICE_ACCOUNT);
+const credentials = JSON.parse(GOOGLE_CREDENTIALS);
+
+exports.handler = async (event, context, callback) => {
+  let { text } = JSON.parse(
+    '{"' + event.body.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
+    (key, value) =>
+      key === ""
+        ? value
+        : decodeURIComponent(value)
+            .replace(/\+/g, " ")
+            .trim()
+  );
+  let [sheetName, ...item] = text.split(" ");
+  let itemNameAndDetail = item.join(" ").split("|");
+
+  try {
+    await appendToSheet({
+      serviceAccount,
+      credentials,
+      sheetId: SHEET_ID,
+      sheetName,
+      data: itemNameAndDetail
+    });
+    callback(null, {
+      statusCode: 200,
+      body: `Successfully added translation for \`${itemNameAndDetail[0]}\` to lore store for language \`${sheetName}\``
+    });
+  } catch (err) {
+    console.error({ err });
+    callback(null, {
+      statusCode: 200,
+      body:
+        "Sorry, failed to add that translation. Maybe that language doesn't have a lore store yet?"
+    });
+  }
+};
