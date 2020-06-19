400afda7daec2bb3f0a9a38a89ab8eac125e074e
Tom Jardine-McNamara <tom@itsravenous.com>
2020-06-19 21:48:48 +0100

    debug

--- functions/inventory-add.js
+++ functions/inventory-add.js
@@ -5,16 +5,93 @@ const {
   INVENTORY_SHEET_ID: SHEET_ID,
   GOOGLE_SERVICE_ACCOUNT,
   GOOGLE_CREDENTIALS,
-  GOOGLE_API_KEY
+  GOOGLE_API_KEY,
+  SLACK_TOKEN
 } = process.env;
 
 const serviceAccount = JSON.parse(GOOGLE_SERVICE_ACCOUNT);
 const credentials = JSON.parse(GOOGLE_CREDENTIALS);
 
 exports.handler = async (event, context, callback) => {
-  const data = getDataFromSlackRequest(event);
+  let { trigger_id } = getDataFromSlackRequest(event);
 
   console.log({ data });
+
+  return await fetch("https://slack.com/api/views.open", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${SLACK_TOKEN}`
+    },
+    body: JSON.stringify({
+      trigger_id,
+      view: {
+        type: "modal",
+        submit: {
+          type: "plain_text",
+          text: "Submit",
+          emoji: true
+        },
+        close: {
+          type: "plain_text",
+          text: "Cancel",
+          emoji: true
+        },
+        title: {
+          type: "plain_text",
+          text: "Add item to inventory",
+          emoji: true
+        },
+        blocks: [
+          {
+            type: "divider"
+          },
+          {
+            type: "input",
+            label: {
+              type: "plain_text",
+              text: "To whose inventory do you wish to add an item?",
+              emoji: true
+            },
+            element: {
+              type: "static_select",
+              placeholder: {
+                type: "plain_text",
+                text: "Select player",
+                emoji: true
+              },
+              options: [
+                {
+                  text: {
+                    type: "plain_text",
+                    text: "Milric",
+                    emoji: true
+                  },
+                  value: "milric"
+                },
+                {
+                  text: {
+                    type: "plain_text",
+                    text: "Fernoca",
+                    emoji: true
+                  },
+                  value: "fernoca"
+                },
+                {
+                  text: {
+                    type: "plain_text",
+                    text: "Sydrel",
+                    emoji: true
+                  },
+                  value: "sydrel"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    })
+  });
   //try {
   //await appendToSheet({
   //serviceAccount,
