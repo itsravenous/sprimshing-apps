a61c8a6f50c593d31c43abc38e023de0d7d7645d
Tom Jardine-McNamara <tom@itsravenous.com>
2020-06-21 14:25:09 +0100

    Bomb out early if payload missing

--- functions/interactions.js
+++ functions/interactions.js
@@ -8,7 +8,16 @@ const RITUNA_CALLBACK_IDS = ["from_vedich", "from_vanlic", "from_breinish"];
 
 exports.handler = async (event, context, callback) => {
   let { text, payload } = getDataFromSlackRequest(event);
-  if (payload) payload = JSON.parse(payload);
+  if (!payload) {
+    // Paylaod required, return error
+    return callback(null, {
+      statusCode: 200,
+      body:
+        "The Sprimshing interaction handler received a request without a payload. This shouldn't happen."
+    });
+  }
+
+  payload = JSON.parse(payload);
   let handler;
 
   if (payload.view && payload.view.callback_id === "inventory_add") {
