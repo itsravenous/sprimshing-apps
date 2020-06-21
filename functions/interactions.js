fe733a633d25a71473da5b25704f7edb46af8613
Tom Jardine-McNamara <tom@itsravenous.com>
2020-06-21 16:55:07 +0100

    debug

--- functions/interactions.js
+++ functions/interactions.js
@@ -20,6 +20,7 @@ exports.handler = async (event, context, callback) => {
   payload = JSON.parse(payload);
   let handler;
 
+  console.log("interaction", payload);
   if (payload.view && payload.view.callback_id === "inventory_add") {
     console.log("handling via inventory add");
     handler = inventoryAddHandler;
