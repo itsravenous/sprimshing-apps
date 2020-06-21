0823ba9db992e67a9c857da4ab8380447a917dad
Tom Jardine-McNamara <tom@itsravenous.com>
2020-06-21 13:59:03 +0100

    select vessels first

--- functions/inventory-add.js
+++ functions/inventory-add.js
@@ -1,9 +1,5 @@
 const fetch = require("node-fetch");
-const {
-  appendToSheet,
-  fetchSheet,
-  getSheetList
-} = require("../google_utils");
+const { appendToSheet, fetchSheet, getSheetList } = require("../google_utils");
 const { getDataFromSlackRequest } = require("../utils");
 const {
   INVENTORY_SHEET_ID: SHEET_ID,
@@ -16,7 +12,92 @@ const {
 const serviceAccount = JSON.parse(GOOGLE_SERVICE_ACCOUNT);
 const credentials = JSON.parse(GOOGLE_CREDENTIALS);
 
-const openModal = async ({ trigger_id }) => {
+const openItemModal = async ({ player, trigger_id }) => {
+  const inventorySheet = await fetchSheet({
+    serviceAccount,
+    credentials,
+    sheetId: SHEET_ID,
+    sheetName: player
+  });
+
+  const vessels = inventorySheet.values[0];
+
+  console.log("got vessels", vessels);
+  const res = await fetch("https://slack.com/api/views.open", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${SLACK_TOKEN}`
+    },
+    body: JSON.stringify({
+      trigger_id,
+      view: {
+        callback_id: "inventory_add/add_item",
+        type: "modal",
+        submit: {
+          type: "plain_text",
+          text: "Submit",
+          emoji: true
+        },
+        close: {
+          type: "plain_text",
+          text: "Cancel",
+          emoji: true
+        },
+        title: {
+          type: "plain_text",
+          text: "Add item to inventory",
+          emoji: true
+        },
+        blocks: [
+          {
+            type: "divider"
+          },
+          {
+            type: "input",
+            block_id: "vessel",
+            label: {
+              type: "plain_text",
+              text: "To which vessel do you wish to add an item?",
+              emoji: true
+            },
+            element: {
+              action_id: "vessel",
+              type: "static_select",
+              placeholder: {
+                type: "plain_text",
+                text: "Select vessel",
+                emoji: true
+              },
+              options: vessels.map(vessel => ({
+                text: {
+                  type: "plain_text",
+                  text: vessel,
+                  emoji: true
+                },
+                value: vessel
+              }))
+            }
+          },
+          {
+            type: "input",
+            block_id: "item",
+            element: {
+              type: "plain_text_input",
+              action_id: "item"
+            },
+            label: {
+              type: "plain_text",
+              text: "Item description",
+              emoji: true
+            }
+          }
+        ]
+      }
+    })
+  });
+};
+const openPlayerModal = async ({ trigger_id }) => {
   const sheets = await getSheetList({
     serviceAccount,
     credentials,
@@ -77,19 +158,6 @@ const openModal = async ({ trigger_id }) => {
                 },
                 value: sheet
               }))
-            }
-          },
-          {
-            type: "input",
-            block_id: "item",
-            element: {
-              type: "plain_text_input",
-              action_id: "item"
-            },
-            label: {
-              type: "plain_text",
-              text: "Item description",
-              emoji: true
             }
           }
         ]
