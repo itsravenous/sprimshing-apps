b3ac5a1d1e069c11eb9ee424be7879c5a546334e
Tom Jardine-McNamara <tom@itsravenous.com>
2020-07-13 23:46:01 +0100

    add short/full name to npcs

--- functions/npcs.js
+++ functions/npcs.js
@@ -1,86 +1,79 @@
-const fetch = require("node-fetch");
-const { SHEET_ID, GOOGLE_API_KEY, SLACK_TOKEN, GM_USERNAME } = process.env;
-
+const fetch = require('node-fetch');
+const { getDataFromSlackRequest } = require('../utils');
+const { fetchSheet } = require('../google-utils');
+const { NPC_SHEET_ID: SHEET_ID, GOOGLE_API_KEY, SLACK_TOKEN, GM_USERNAME } = process.env;
+console.log(process.env);
 // Fetches NPCs from shared Google sheet
 const fetchNpcs = async () => {
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/npcs?key=${GOOGLE_API_KEY}`;
-  const res = await fetch(url);
-  const { values } = await res.json();
-  return (
-    values
-      // Remove header row
-      .slice(1)
-      // Filter incompleye/empty rows
-      .filter(entry => entry[0] && entry[1])
-  );
+	const rows = await fetchSheet({ sheetId: SHEET_ID });
+	return (
+		rows
+			// Remove header row
+			.slice(1)
+			// Filter incomplete/empty rows
+			.filter((entry) => entry[0] && entry[1])
+	);
 };
 
-const sendMessage = async ({ channel, name, image, text }) => {
-  return await fetch("https://slack.com/api/chat.postMessage", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${SLACK_TOKEN}`
-    },
-    body: JSON.stringify({
-      channel,
-      text,
-      icon_url: image,
-      username: name,
-      link_names: true
-    })
-  });
-};
+const sendMessage = ({ channel, name, image, text }) =>
+	fetch('https://slack.com/api/chat.postMessage', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${SLACK_TOKEN}`,
+		},
+		body: JSON.stringify({
+			channel,
+			text,
+			icon_url: image,
+			username: name,
+			link_names: true,
+		}),
+	});
 
 // Export sendMessage for testing
 exports.sendMessage = sendMessage;
 
 // This is a specially-named function automatically called by AWS
 exports.handler = async (event, context, callback) => {
-  // Parse out data passed by Slack
-  const { user_name, text, channel_id } = JSON.parse(
-    '{"' + event.body.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
-    (key, value) =>
-      key === "" ? value : decodeURIComponent(value).replace(/\+/g, " ")
-  );
+	// Parse out data passed by Slack
+	const { user_name, text, channel_id } = getDataFromSlackRequest(event);
 
-  // Restrict use to GM user
-  if (user_name !== GM_USERNAME) {
-    return callback(null, {
-      statusCode: 200,
-      body: "Only the GM can use this command"
-    });
-  }
+	// Restrict use to GM user if defined
+	if (GM_USERNAME && user_name !== GM_USERNAME) {
+		return callback(null, {
+			statusCode: 200,
+			body: 'Only the GM can use this command',
+		});
+	}
 
-  // Parse NPC name and message
-  const [npcName, ...words] = text.split(" ");
+	// Parse NPC name and message
+	const [npcName, ...words] = text.split(' ');
 
-  // Fetch NPCs stored in Google sheet
-  const npcs = await fetchNpcs();
+	// Fetch NPCs stored in Google sheet
+	const npcs = await fetchNpcs();
 
-  // Find requested NPC in sheet
-  const npc = npcs.find(
-    entry => entry[0].toLowerCase() === npcName.toLowerCase()
-  );
+	// Find requested NPC in sheet
+	const npc = npcs.find((entry) => entry[0].toLowerCase() === npcName.toLowerCase());
 
-  let body;
-  if (npc) {
-    // NPC found, send message
-    const [name, image] = npc;
-    await sendMessage({
-      channel: channel_id,
-      name,
-      image,
-      text: words.join(" ")
-    });
-  } else {
-    // NPC not found, tell user
-    body = "No such NPC!";
-  }
+	let body = '';
+	if (npc) {
+		// NPC found, send message
+		const [_, fullName, image] = npc;
+		const res = await sendMessage({
+			channel: channel_id,
+			name: fullName,
+			image,
+			text: words.join(' '),
+		});
+	} else {
+		// NPC not found, tell user
+		body = 'No such NPC!';
+	}
 
-  // Send response back to Slack
-  callback(null, {
-    statusCode: 200,
-    body
-  });
+	// Send response back to Slack
+	callback(null, {
+		statusCode: 200,
+		body,
+	});
 };
