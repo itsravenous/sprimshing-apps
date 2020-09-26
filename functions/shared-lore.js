eacd6271428516fda44c74ae79d54f453ac03eee
Tom Jardine-McNamara <tom@itsravenous.com>
2020-09-26 22:49:55 +0100

    feat: add old sprimshing knowledge function as new "shared lore" function
    the miami nights style modal knowledge is fundamentally incompatible with the old style

---
+++ functions/shared-lore.js
@@ -0,0 +1,85 @@
+const { fetchSheet } = require("../google-utils");
+const { getDataFromSlackRequest } = require("../utils");
+const { KNOWLEDGE_SHEET_ID: SHEET_ID } = process.env;
+
+const fetchKnowledge = async sheetName => {
+  try {
+    const rows = await fetchSheet({
+      sheetId: SHEET_ID,
+      sheetName
+    });
+    return (
+      rows
+        // Filter rows with nothing in col 1 (this should be the key)
+        .filter(entry => entry[0])
+    );
+  } catch (err) {
+    console.error({ err });
+    return;
+  }
+};
+
+const listKnowledge = async sheetname => {
+  let knowledge;
+  try {
+    knowledge = await fetchKnowledge(sheetname);
+  } catch (err) {
+    console.error({ err });
+    return "Nothing is known about the subject `" + sheetname + "`";
+  }
+
+  let header = knowledge[0][0];
+  console.log({ header });
+  let result = knowledge
+    .slice(1)
+    .map(element => element[0])
+    .join(", ");
+  return header + ": " + result;
+};
+
+const detailKnowledge = async (sheetname, itemname) => {
+  let knowledge;
+  try {
+    knowledge = await fetchKnowledge(sheetname);
+  } catch {
+    return "Nothing is known about the subject `" + sheetname + "`";
+  }
+
+  let header = knowledge[0];
+  let item = knowledge
+    .slice(1)
+    .find(element => element[0].toLowerCase() === itemname.toLowerCase());
+
+  if (!item) {
+    return (
+      "Nothing is known about `" +
+      itemname +
+      "` (at least not in the topic of `" +
+      sheetname +
+      "`)"
+    );
+  }
+  var r = "";
+  var i;
+  for (i = 0; i < header.length; i++) {
+    r += header[i] + ": " + item[i] + "\n";
+  }
+  return r;
+};
+
+exports.handler = async (event, context, callback) => {
+  const { text } = getDataFromSlackRequest(event);
+  let [dictionaryName, ...entryName] = text.split(" ");
+  entryName = entryName.join(" ");
+  let response;
+  if (entryName) {
+    response = await detailKnowledge(dictionaryName, entryName);
+  } else {
+    response = await listKnowledge(dictionaryName);
+  }
+
+  callback(null, {
+    statusCode: 200,
+    body: response
+  });
+};
