5875d83a92f8cd5cbab1909f1f19bcb698fde611
Tom Jardine-McNamara <tom@itsravenous.com>
2021-01-28 22:32:14 +0000

    debug

--- google-utils.js
+++ google-utils.js
@@ -6,6 +6,7 @@ const SCOPES = [
   "https://www.googleapis.com/auth/documents",
 ];
 
+console.log("GOOGLE_SERVICE_ACCOUNT", process.env.GOOGLE_SERVICE_ACCOUNT);
 const defaultServiceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
 
 // Create auth from service account
